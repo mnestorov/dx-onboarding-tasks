c51c74960a7bc01957a5b303605479bb8e44844f
Martin Nestorov <mbnestorov@gmail.com>
2021-11-10 14:52:52 +0200

    Adding custom page template | Asana task: https://app.asana.com/0/1201345304239951/1201345230458139/f

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -6,11 +6,16 @@
  * @package WordPress
  */
 
-/**
- *  Enqueue scripts and style from parent theme
- */
-function twentytwentyone_styles() {
-	wp_enqueue_style( 'child-style', get_stylesheet_uri(), false, false, 'all' );
+if ( ! function_exists( 'twentytwentyone_child_styles' ) ) {
+	/**
+	 *  Enqueue scripts and style from parent theme
+	 */
+	function twentytwentyone_child_styles() {
+		// Extending the 'twentytwentyone-style' for the Twenty Twenty One theme.
+		wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', false, wp_get_theme()->parent()->get( 'Version' ) );
+		// Adding the child theme style.
+		wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ), false, 'all' );
+	}
 }
 
-add_action( 'wp_enqueue_scripts', 'twentytwentyone_styles' );
+add_action( 'wp_enqueue_scripts', 'twentytwentyone_child_styles' );
---
+++ wp-content/themes/twentytwentyone-child/page-templates/full-width-template.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: Full Width Page
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+?>
+
+<div class="entry-content">
+	<main class="site-main" role="main">
+
+	<?php if ( has_post_thumbnail() ) : ?>
+
+		<?php the_post_thumbnail(); ?>
+
+	<?php else : ?>
+
+		<!-- Dixy image -->
+		<div class="centered-image">
+			<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/dixy.png'; ?>" title="Dixy" alt="image" width="250">
+		</div>
+
+	<?php endif; ?>
+
+	<?php the_content(); ?>
+
+	</main><!-- .site-main -->
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
