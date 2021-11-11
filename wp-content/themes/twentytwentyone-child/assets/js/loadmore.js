e6af24de2966f68fbfee9703eb6aafc9ba88d810
Martin Nestorov <mbnestorov@gmail.com>
2021-11-11 15:49:19 +0200

    Minor changes made after feedback | Asana task: https://app.asana.com/0/1201345304239951/1201345383960686/f

--- wp-content/themes/twentytwentyone-child/assets/js/loadmore.js
+++ wp-content/themes/twentytwentyone-child/assets/js/loadmore.js
@@ -1,33 +1,35 @@
-jQuery(function($){
-	$('.loadmore').click(function(){
- 
-		var button = $(this),
-		    data = {
-			'action': 'loadmore',
-			'query': loadmore_params.posts, // get params from wp_localize_script() function
-			'page' : loadmore_params.current_page
-		};
- 
-		$.ajax({
-			url : loadmore_params.ajaxurl, // AJAX handler
-			data : data,
-			type : 'POST',
-			beforeSend : function ( xhr ) {
-				button.text('Loading...'); 
-			},
-			success : function( data ){
-				if( data ) { 
-					button.text( 'Load More Posts' ).prev().before(data); // insert new posts
-					loadmore_params.current_page++;
- 
-					if ( loadmore_params.current_page == loadmore_params.max_page ) {
-                        button.remove(); // if last page, remove the button
-                    }
-						
-				} else {
-					button.remove(); // if no data, remove the button
+jQuery(document).ready(function ($) {
+	$(function($){
+		$('.loadmore').click(function(){
+	
+			let button = $(this),
+				data = {
+				'action': 'loadmore',
+				'query': loadmore_params.posts, // get params from wp_localize_script() function
+				'page' : loadmore_params.current_page
+			};
+	
+			$.ajax({
+				url : loadmore_params.ajaxurl, // AJAX handler
+				data : data,
+				type : 'POST',
+				beforeSend : function ( xhr ) {
+					button.text('Loading...'); 
+				},
+				success : function( data ){
+					if( data ) { 
+						button.text( 'Load More Posts' ).prev().before(data); // insert new posts
+						loadmore_params.current_page++;
+	
+						if ( loadmore_params.current_page == loadmore_params.max_page ) {
+							button.remove(); // if last page, remove the button
+						}
+							
+					} else {
+						button.remove(); // if no data, remove the button
+					}
 				}
-			}
+			});
 		});
 	});
 });
