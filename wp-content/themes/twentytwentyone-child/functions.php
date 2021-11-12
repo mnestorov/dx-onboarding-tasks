d45602b27be7aaf2d64038882bf84f7ae93aa201
Martin Nestorov <mbnestorov@gmail.com>
2021-11-12 18:36:26 +0200

    Added menu only for logged-in users. Send email to admin on user profile update. | Asana task: https://app.asana.com/0/1201345304239951/1201345230350868/f

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -70,3 +70,55 @@ if ( ! function_exists( 'twentytwentyone_child_loadmore_ajax_handler' ) ) {
 
 add_action( 'wp_ajax_loadmore', 'twentytwentyone_child_loadmore_ajax_handler' );
 add_action( 'wp_ajax_nopriv_loadmore', 'twentytwentyone_child_loadmore_ajax_handler' );
+
+if ( ! function_exists( 'twentytwentyone_child_nav_menu_args' ) ) {
+	/**
+	 *  Display different menus to logged-in users
+	 */
+	function twentytwentyone_child_nav_menu_args( $args = '' ) {
+
+		if ( is_user_logged_in() ) {
+			$args['menu'] = 'Logged-In';
+		} else {
+			$args['menu'] = 'Primary Menu';
+		}
+
+		return $args;
+	}
+
+	add_filter( 'wp_nav_menu_args', 'twentytwentyone_child_nav_menu_args' );
+}
+
+if ( ! function_exists( 'twentytwentyone_child_user_profile_update' ) ) {
+	/**
+	 * Send email to admin if user updated his profile
+	 */
+	function twentytwentyone_child_user_profile_update( $user_id ) {
+
+		$user_info = get_userdata( $user_id );
+
+		$to       = get_option( 'admin_email' );
+		$subject  = 'User Profile Updated';
+		$message  = "Hello Administrator,\n\nThe " . $user_info->user_nicename . " (" . $user_info->user_email . ") profile has been updated! \n\n";
+		$message .= "Site URL: " . get_bloginfo( 'wpurl' ) . "\n\n";
+		$message .= "Regards, \n" . get_option( 'blogname' );
+
+		wp_mail( $to, $subject, $message );
+	}
+}
+
+add_action( 'profile_update', 'twentytwentyone_child_user_profile_update', 10, 2 );
+
+/**
+ *  TESTING :: email testing on local enviroment
+ */
+function mailtrap( $phpmailer ) {
+	$phpmailer->isSMTP();
+	$phpmailer->Host 	 = 'smtp.mailtrap.io';
+	$phpmailer->SMTPAuth = true;
+	$phpmailer->Port 	 = 2525;
+	$phpmailer->Username = '68b2c86d96e500';
+	$phpmailer->Password = '79bcdfccbde0b5';
+}
+
+add_action( 'phpmailer_init', 'mailtrap' );
