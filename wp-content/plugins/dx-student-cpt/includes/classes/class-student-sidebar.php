1cc5e1de49b33a79b56f0937245ce59cc7235b8c
Martin Nestorov <mbnestorov@gmail.com>
2021-11-22 15:06:39 +0200

    Display the sidebar in the post content right after the student shortcode | Asana task: https://app.asana.com/0/1201345304239951/1201345346705641/f

--- wp-content/plugins/dx-student-cpt/includes/classes/class-student-sidebar.php
+++ wp-content/plugins/dx-student-cpt/includes/classes/class-student-sidebar.php
@@ -14,8 +14,9 @@ if ( ! class_exists( 'StudentSidebar' ) ) {
 		 */
 		public function __construct() {
 			add_action( 'widgets_init', array( $this, 'dx_students_sidebar' ) );
+			add_filter( 'the_content', array( $this, 'dx_add_sidebar_before' ), 7 );
 		}
-		
+
 		/**
 		 * Registers the students custom sidebar
 		 */
@@ -27,6 +28,22 @@ if ( ! class_exists( 'StudentSidebar' ) ) {
 					'description' => __( 'This sidebar will display the students widget.' ),
 				)
 			);
+		}
+
+		/**
+		 * Adding the students custom sidebar before post content
+		 */
+		public function dx_add_sidebar_before( $content ) {
+			if ( is_single() && is_active_sidebar( 'students_sidebar' ) ) {
+
+				ob_start();
+
+				dynamic_sidebar( 'students_sidebar' );
+
+				$content .= ob_get_clean();
+			}
+
+			return $content;
 		}
 	}
 }
