b92dfeaf01831ceeecd0f181bccbd44283fbe463
Martin Nestorov <mbnestorov@gmail.com>
2021-11-23 18:41:50 +0200

    Remove and move unnecessary files to students cpt.

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -18,64 +18,6 @@ if ( ! function_exists( 'twentytwentyone_child_styles' ) ) {
 
 add_action( 'wp_enqueue_scripts', 'twentytwentyone_child_styles' );
 
-if ( ! function_exists( 'twentytwentyone_child_scripts' ) ) {
-	/**
-	 *  Enqueue scripts for the child theme
-	 */
-	function twentytwentyone_child_scripts() {
-		global $wp_query;
-
-		// Register script but don't enqueue it yet.
-		wp_register_script( 'child-loadmore', get_stylesheet_directory_uri() . '/assets/js/loadmore.js', array( 'jquery' ), false, true );
-		wp_enqueue_script( 'child-loadmore' );
-
-		wp_localize_script(
-			'child-loadmore',
-			'loadmore_params',
-			array(
-				'ajaxurl'      => site_url() . '/wp-admin/admin-ajax.php', // WP AJAX.
-				'posts'        => wp_json_encode( $wp_query->query_vars ),    // Loop is here.
-				'current_page' => get_query_var( 'paged' ) ?? 1,
-				'max_page'     => $wp_query->max_num_pages,
-			)
-		);
-
-		wp_register_style( 'child-main', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), false, 'all' );
-		wp_enqueue_style( 'child-main' );
-	}
-}
-
-add_action( 'wp_enqueue_scripts', 'twentytwentyone_child_scripts' );
-
-if ( ! function_exists( 'twentytwentyone_child_loadmore_ajax_handler' ) ) {
-	/**
-	 *  Load More ajax handler
-	 */
-	function twentytwentyone_child_loadmore_ajax_handler() {
-
-		$args                     = json_decode( stripslashes( ! isset( $_POST['query'] ) ?? '' ), true );
-		$args['post_type']        = 'student';
-		$args['posts_per_page']   = 3;
-		$args['paged']            = $_POST['page'] + 1; // Loading next page.
-		$args['post_status']      = 'publish';
-		$args['suppress_filters'] = true;
-
-		$query = new WP_Query( $args );
-
-		if ( $query->have_posts() ) {
-			while ( $query->have_posts() ) {
-				$query->the_post();
-				get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
-			}
-		}
-
-		die;
-	}
-}
-
-add_action( 'wp_ajax_loadmore', 'twentytwentyone_child_loadmore_ajax_handler' );
-add_action( 'wp_ajax_nopriv_loadmore', 'twentytwentyone_child_loadmore_ajax_handler' );
-
 if ( ! function_exists( 'twentytwentyone_child_nav_menu_args' ) ) {
 	/**
 	 *  Display different menus to logged-in users
