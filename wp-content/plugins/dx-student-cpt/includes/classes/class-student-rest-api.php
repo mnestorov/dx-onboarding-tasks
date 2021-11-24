e6fe3703b57eb00beb7d8a5d9c4504e9e58381b3
Martin Nestorov <mbnestorov@gmail.com>
2021-11-24 17:38:59 +0200

    Add three more endpoints add/edit/delete and needed security accordingly | Asana task: https://app.asana.com/0/1201345304239951/1201345229184408/f

--- wp-content/plugins/dx-student-cpt/includes/classes/class-student-rest-api.php
+++ wp-content/plugins/dx-student-cpt/includes/classes/class-student-rest-api.php
@@ -20,12 +20,18 @@ if ( ! class_exists( 'StudentRestApi' ) ) {
 		 * Callback for getting data for all students
 		 */
 		public function dx_get_all_student_data() {
-			$posts = get_posts(
-				array(
-					'post_type'     => 'student',
-					'post_per_page' => 3,
-				)
+
+			$args = array(
+				'post_type'     => 'student',
+				'post_per_page' => 3,
+				'post_status'   => 'publish',
 			);
+
+			$posts = get_posts( $args );
+
+			if ( empty( $posts ) ) {
+				return null;
+			}
 
 			return rest_ensure_response( $posts );
 		}
@@ -34,13 +40,40 @@ if ( ! class_exists( 'StudentRestApi' ) ) {
 		 * Callback for getting data for one student
 		 */
 		public function dx_get_one_student_data( $request ) {
-			$id = (int) $request['id'];
+			$post_id = intval( $request['id'] );
 
-			$post = get_post( $id );
+			$post = get_post( $post_id );
 
 			if ( empty( $post ) ) {
-				return rest_ensure_response( array() );
+				return null;
 			}
+
+			return rest_ensure_response( $post );
+		}
+
+		/**
+		 * Callback for adding a new student
+		 */
+		public function dx_add_new_student_data( $request ) {
+			$post_id = wp_insert_post( json_decode( $request->get_body() ) );
+
+			return rest_ensure_response( $post_id );
+		}
+
+		/**
+		 * Callback for registering the update route
+		 */
+		public function dx_edit_student( $request ) {
+			$post_id = wp_update_post( json_decode( $request->get_body() ) );
+
+			return rest_ensure_response( $post_id );
+		}
+
+		/**
+		 * Callback for deleting a student by ID
+		 */
+		public function dx_delete_student_by_id( $request ) {
+			$post = wp_delete_post( $request['id'] );
 
 			return rest_ensure_response( $post );
 		}
@@ -49,7 +82,7 @@ if ( ! class_exists( 'StudentRestApi' ) ) {
 		 * Registers the custom endpoints
 		 */
 		public function dx_register_api_endpoints() {
-			register_rest_route(
+			register_rest_route( // Get all students.
 				'/api/v1',
 				'/students',
 				array(
@@ -58,12 +91,48 @@ if ( ! class_exists( 'StudentRestApi' ) ) {
 				)
 			);
 
-			register_rest_route(
+			register_rest_route( // Get single student.
 				'/api/v1',
 				'/students/(?P<id>[\d]+)',
 				array(
 					'methods'  => 'GET',
 					'callback' => array( $this, 'dx_get_one_student_data' ),
+				)
+			);
+
+			register_rest_route( // Add new student.
+				'/api/v1',
+				'/students/add',
+				array(
+					'methods'             => 'POST',
+					'callback'            => array( $this, 'dx_add_new_student_data' ),
+					'permission_callback' => function() {
+						return current_user_can( 'edit_others_posts' );
+					},
+				)
+			);
+
+			register_rest_route( // Edit student by ID.
+				'/api/v1',
+				'/students/edit/(?P<id>[\d]+)',
+				array(
+					'methods'             => 'PUT',
+					'callback'            => array( $this, 'dx_edit_student' ),
+					'permission_callback' => function() {
+						return current_user_can( 'edit_others_posts' );
+					},
+				)
+			);
+
+			register_rest_route( // Delete student by ID.
+				'/api/v1',
+				'/students/delete/(?P<id>[\d]+)',
+				array(
+					'methods'             => 'DELETE',
+					'callback'            => array( $this, 'dx_delete_student_by_id' ),
+					'permission_callback' => function() {
+						return current_user_can( 'edit_others_posts' );
+					},
 				)
 			);
 		}
